c71be5b40ba6329db415a34b677d96423f6fa158
SafaElmali <safaelmali3@gmail.com>
2019-01-28 23:30:34 +0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    new Typed('.firstText', {
+        strings: ["Hello, I'm Safa 👋"],
+        typeSpeed: 20,
+        startDelay: 900,
+        showCursor: false
+      });
+    }),
+    new Typed('.personalInfo', {
+        strings: ["I'm Junior Software Developer(Well at least trying to be!).<br> I love to make web and mobile applications!👨‍💻"],
+        typeSpeed: 20,
+        startDelay: 1800,
+        showCursor: false
+    }),
+    new Typed('.mediaInfoStart', {
+        strings: ["You can check out my"],
+        typeSpeed: 30,
+        startDelay: 5500,
+        showCursor: false
+    }),
+    new Typed('.mediaInfoEnd', {
+        strings: ["By clicking the icon down below"],
+        typeSpeed: 20,
+        startDelay: 7000,
+        showCursor: false
+    }),
+    new Typed('.mailInfo', {
+        strings: ["Get in touch <a href='mailto:'>tsafaelmali@gmail.com</a><span class='cursor'>_</span>"],
+        typeSpeed: 20,
+        startDelay: 8500,
+        showCursor: false
+    });
